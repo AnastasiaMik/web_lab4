dd64cbd1b3c0930b3fee4727fdad3ace30cbb465
AnastasiaMik <Anastasia081998@gmail.com>
2019-04-25 23:03:39 +0300

    Create articles_admin.php

---
+++ views/articles_admin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+	
+    <title>Новостной блог</title>
+</head>
+<body>
+  <div>
+    <table>
+      <tr>
+        <th>Дата создания</th>
+        <th>Заголовок</th>
+        <th></th>
+        <th></th>
+      </tr>
+    </table>
+ </div>
+</div>
+</body>
+</html>
