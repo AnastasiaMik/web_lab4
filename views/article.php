de8798be78d49a6e6dc610f54687c58cb0791e45
AnastasiaMik <Anastasia081998@gmail.com>
2019-04-25 21:28:55 +0300

    Create article.php

---
+++ views/article.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+<html>
+
+<head>
+    <meta charset="utf-8"> 
+	
+    <title> Новостной портал </title>
+</head>
+<body>
+    <div class="container"> 
+        <h1> Новостной портал </h1>
+        <div> 
+            <div class="article">
+                <em> Опубликовано: </em>     
+            </div>
+        </div>     
+    </div>
+</body>
+    
+</html>
