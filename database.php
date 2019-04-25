4f57ea7c15390866f7d25b6cf4d358290a692631
AnastasiaMik <Anastasia081998@gmail.com>
2019-04-25 21:14:53 +0300

    Create database.php

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php 
+define('MYSQL_SERVER', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', 'rootroot');
+define('MYSQL_DB', 'blog');
+
+    $link = mysqli_connect(MYSQL_SERVER, MYSQL_USER,MYSQL_PASSWORD, MYSQL_DB);    
+?>
