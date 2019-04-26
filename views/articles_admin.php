e1247c8f7c747da25edd8f567be0044e38d5654c
AnastasiaMik <Anastasia081998@gmail.com>
2019-04-26 16:39:39 +0300

    Update articles_admin.php

--- views/articles_admin.php
+++ views/articles_admin.php
@@ -6,16 +6,30 @@
     <title>Новостной блог</title>
 </head>
 <body>
-  <div>
-    <table>
-      <tr>
-        <th>Дата создания</th>
-        <th>Заголовок</th>
-        <th></th>
-        <th></th>
-      </tr>
-    </table>
- </div>
-</div>
+    <div class = "container">
+        <a href="../index.php"><h1>На главную</h1></a>
+        <a href="index.php?action=add">Добавить статью</a>
+        <div class = "container">
+            <table>
+                <tr>
+                    <th>Дата создания</th>
+                    <th>Заголовок</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <?php foreach ($articles as $a): ?> 
+                <tr>
+                    <td><?=$a['date']?></td>
+                    <td><?=$a['title']?></td>
+                    <td>
+                        <a href="index.php?action=edit&id=<?=$a['id']?>">Редактировать</a>
+                    </td>
+                    <td>
+                        <a href="index.php?action=delete&id=<?=$a['id']?>">Удалить</a>
+                    </td>
+                </tr>
+                <?php endforeach ?> 
+            </table>
+        </div>
+    </div>
 </body>
-</html>
