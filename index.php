44cab9bf99224d01895925afde159a7285179170
AnastasiaMik <Anastasia081998@gmail.com>
2019-04-26 14:30:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
     // Подключение файлов 
     require_once 'database.php';
+    require_once 'models/articles.php';
     $link = mysqli_connect(MYSQL_SERVER, MYSQL_USER,MYSQL_PASSWORD, MYSQL_DB); 
+    $articles = articles_all($link); 
+    include("views/articles.php");
 ?>
