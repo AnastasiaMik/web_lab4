d0c34af12dc4114683856e2aebe447b15e962dd7
AnastasiaMik <Anastasia081998@gmail.com>
2019-04-26 14:31:31 +0300

    Update article.php

--- article.php
+++ article.php
@@ -1,5 +1,9 @@
 <?php
     require_once 'database.php';
+    require_once 'models/articles.php';
     
     $link = mysqli_connect(MYSQL_SERVER, MYSQL_USER,MYSQL_PASSWORD, MYSQL_DB);   
-?> 
+    $article = articles_get($link, $_GET['id']);
+    
+    include("views/article.php")
+?>
